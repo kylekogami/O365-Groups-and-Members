ee229f2b07cc834f76cd85da282996117e8f6421
kylekogami <60519319+kylekogami@users.noreply.github.com>
2023-07-25 15:22:57 -0700

    Add files via upload

---
+++ SharedMailboxes.ps1
@@ -0,0 +1,12 @@
+﻿#Install-Module ExchangePowerShell
+
+Import-Module PSExcel
+Import-Module AzureADPreview
+Import-Module ExchangePowerShell
+mkdir C:\scripts
+
+#Connect to Exchange Online
+Connect-ExchangeOnline -ShowBanner:$False
+
+
+Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited | select Identity,PrimarySmtpAddress,User | Where-Object {($_.user -like '*@*')}|Export-Csv C:\scripts\SharedMailboxes.csv  -NoTypeInformation 
